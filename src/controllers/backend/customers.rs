95f7532aea3039a23ca52b058931e61bd42e2f2a
Patrice André <mail@patriceandre.com>
2021-02-01 00:00:53 +0100

    feat(style): add customer form

--- src/controllers/backend/customers.rs
+++ src/controllers/backend/customers.rs
@@ -1,26 +1,34 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
+use serde::Deserialize;
 use askama::Template;
 
 #[derive(Template)]
 #[template(path = "backend/customers/index.html")]
 struct IndexTemplate {}
 
-#[derive(Template)]
-#[template(path = "backend/customers/new.html")]
-struct NewTemplate {}
-
 pub async fn index() -> impl Responder {
     let index = IndexTemplate {};
     HttpResponse::Ok().body(index.render().unwrap())
 }
+
+#[derive(Template)]
+#[template(path = "backend/customers/new.html")]
+struct NewTemplate {}
 
 pub async fn new() -> impl Responder {
     let new = NewTemplate {};
     HttpResponse::Ok().body(new.render().unwrap())
 }
 
-pub async fn create() -> impl Responder {
-    HttpResponse::Ok().body("")
+#[derive(Deserialize)]
+pub struct FormData {
+    first_name: String,
+    last_name: String
+}
+
+pub async fn create(form: web::Form<FormData>) -> impl Responder {
+    log::info!("{} {}", form.first_name, form.last_name);
+    HttpResponse::Ok().body(format!("{} {}", form.first_name, form.last_name))
 }
 
 pub async fn edit() -> impl Responder {
